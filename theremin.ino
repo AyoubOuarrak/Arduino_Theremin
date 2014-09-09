b473aed8f97c2bda8ce4e2f9cc7cbdd65d04d916
Ayoub Ouarrak <andreyvondorcon@gmail.com>
2014-09-09 17:34:21 +0200

    first commit

---
+++ theremin.ino
@@ -0,0 +1,42 @@
+
+
+#include <Ultrasonic.h>
+
+#define TRIGGER_PIN  12
+#define ECHO_PIN     13
+
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);
+  
+}
+
+void loop() {
+  float cmMsec;
+  long freq;
+  long microsec = ultrasonic.timing();
+
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  
+  freq = map(cmMsec, 2, 160, 2600, 10);
+  buzz(8, freq, 10);
+
+  delay(20);
+}
+
+void buzz(int targetPin, long frequency, long length) {
+  long delayValue = 1000000/frequency/2; // calculate the delay value between transitions
+  //// 1 second's worth of microseconds, divided by the frequency, then split in half since
+  //// there are two phases to each cycle
+  long numCycles = frequency * length/ 1000; // calculate the number of cycles for proper timing
+  //// multiply frequency, which is really cycles per second, by the number of seconds to 
+  //// get the total number of cycles to produce
+  for (long i=0; i < numCycles; i++){ // for the calculated length of time...
+    digitalWrite(targetPin,HIGH); // write the buzzer pin high to push out the diaphram
+    delayMicroseconds(delayValue); // wait for the calculated delay value
+    digitalWrite(targetPin,LOW); // write the buzzer pin low to pull back the diaphram
+    delayMicroseconds(delayValue); // wait againf or the calculated delay value
+  }
+}
